1b9b42b9934d1b0d1c26288fdfbc05530b227f7a
BakhramovDev <bahramovhusein@gmail.com>
2023-12-12 12:25:43 +0500

    fhfhf

--- src/router/index.js
+++ src/router/index.js
@@ -29,16 +29,6 @@ const routes = [
       return import('../views/Restaurants.vue')
     }
   },
-  {
-    path: '/About',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import('../views/About.vue')
-    }
-  },
 ]
 
 const router = createRouter({
